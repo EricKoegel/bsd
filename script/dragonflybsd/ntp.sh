1dee04e7cde1ff5fd4b1369d123e148cc04eedad
Eric Koegel <eric.koegel@gmail.com>
2017-10-20 08:22:57 +0300

    Minor changes to the DFly build
    So that it works when hooked up to Vagrant+Ansible+Jenkins.

--- script/dragonflybsd/ntp.sh
+++ script/dragonflybsd/ntp.sh
@@ -2,3 +2,6 @@
 
 echo "==> Install NTP";
 pkg install -y ntp;
+
+echo "==> Reinstall python, base ISO version is broken"
+cd /usr && make dports-create-shallow && cd /usr/dports/lang/python27 && make reinstall clean && ln -s /usr/local/bin/python2.7 /usr/local/bin/python
--- script/freebsd/postinstall.sh
+++ script/freebsd/postinstall.sh
@@ -1,9 +1,5 @@
 #!/bin/sh -u
 
-echo "==> Setup NTP";
-# Set the time correctly
-ntpdate -v -b in.pool.ntp.org;
-
 echo "==> Install curl, ca_root_nss and sudo";
 # Install sudo and curl
 pkg install -y curl;
@@ -17,14 +13,11 @@ ln -sf /usr/local/share/certs/ca-root-nss.crt /etc/ssl/cert.pem;
 echo "==> Enable NFS and set hostname";
 # As sharedfolders are not in defaults ports tree, we will use NFS sharing
 cat >>/etc/rc.conf << RC_CONF
+vboxguest_enable="YES"
+vboxservice_enable="YES"
 rpcbind_enable="YES"
 nfs_server_enable="YES"
 mountd_flags="-r"
 hostname="$HOSTNAME"
 RC_CONF
 
-echo "==> Don't build for X11";
-# Disable X11 because Vagrants VMs are (usually) headless
-cat >>/etc/make.conf << MAKE_CONF
-WITHOUT_X11="YES"
-MAKE_CONF
